d8da93cd7e3356c09b3c85439ffed60b204c9c7c
Sofy Amartya <sofy_amartya@yahoo.com>
2020-07-30 19:56:09 +0700

    first step

---
+++ animal.php
@@ -0,0 +1,11 @@
+<?php
+  class Animal {
+    public $legs = 4;
+    public cold_blooded = false;
+    public $name;
+
+    public function _construct($name) {
+      $this->name = $name;
+    }
+  }
+?>
